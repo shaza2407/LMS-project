2aa8374aa37e75d435ee43c7c815a72f7ec98a16
Muhammad Hasan <modyhassan917@gmail.com>
2024-12-19 15:01:21 +0200

    Notifications

--- src/main/java/com/example/lms/service/NotificationService.java
+++ src/main/java/com/example/lms/service/NotificationService.java
@@ -1,27 +1,35 @@
 package com.example.lms.service;
 
 import com.example.lms.model.Notification;
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.List;
-
-public class NotificationService {
+@Service
+public class NotificationService
+{
     private final List<Notification> notifications = new ArrayList<>();
 
-    public List<Notification> getAllNotifications(String role) {//this should be an id i guess not role based
+    public List<Notification> getAllNotifications(String role)
+    {//this should be an id i guess not role based
         List<Notification> filteredNotifications = new ArrayList<>();
-        for (Notification notification : notifications) {
-            if (notification.getRecipientRole().equals(role)) {
+        for (Notification notification : notifications)
+        {
+            if (notification.getRecipientRole().equals(role))
+            {
                 filteredNotifications.add(notification);
             }
         }
         return filteredNotifications;
     }
 
-    public void addNotification(Notification notification) {
+    public void addNotification(Notification notification)
+    {
         notifications.add(notification);
     }
 
-    public void markNotificationAsRead(Long id) {
+    public void markNotificationAsRead(Long id)
+    {
         notifications.stream()
                 .filter(notification -> notification.getId().equals(id))
                 .forEach(Notification::markAsRead);
