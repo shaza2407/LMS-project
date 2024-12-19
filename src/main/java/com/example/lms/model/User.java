880d1cbe6583a55c26cb8c4bb10d2b9a11543820
yousef-showman <123179662+yousef-showman@users.noreply.github.com>
2024-12-19 15:32:26 +0200

    simple constructor

--- src/main/java/com/example/lms/model/User.java
+++ src/main/java/com/example/lms/model/User.java
@@ -9,8 +9,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@NoArgsConstructor
-@AllArgsConstructor
+
 @Getter
 @Setter
 @Entity
@@ -22,26 +21,15 @@ public class User {
     private String email;
     private String role; // "Admin", "Instructor", "Student"
 
-//    // Constructor
-//    public User() {}
+    // Constructor
+    public User() {}
 
-    //    public User(Long id, String name, String email, String role) {
-//        this.id = id;
-//        this.name = name;
-//        this.email = email;
-//        this.role = role;
-//    }
+        public User(Long id, String name, String email, String role) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.role = role;
+    }
 
-    // Getters and Setters
-//    public Long getId() { return id; }
-//    public void setId(Long id) { this.id = id; }
-//
-//    public String getName() { return name; }
-//    public void setName(String name) { this.name = name; }
-//
-//    public String getEmail() { return email; }
-//    public void setEmail(String email) { this.email = email; }
-//
-//    public String getRole() { return role; }
-//    public void setRole(String role) { this.role = role; }
+
 }
