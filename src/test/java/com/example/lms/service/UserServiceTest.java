cc3a6fc5ca318f2df33655e5a656866371e05e3b
yousef-showman <123179662+yousef-showman@users.noreply.github.com>
2024-12-20 11:26:54 +0200

    update automatically

--- src/test/java/com/example/lms/service/UserServiceTest.java
+++ src/test/java/com/example/lms/service/UserServiceTest.java
@@ -9,7 +9,6 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.Arrays;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
