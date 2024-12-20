1f35146c48a31f5707a3246a1655b5eebb8ce3c0
yousef-showman <123179662+yousef-showman@users.noreply.github.com>
2024-12-20 11:23:25 +0200

    make the application scan the DB

--- src/main/java/com/example/lms/LMSApplication.java
+++ src/main/java/com/example/lms/LMSApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(scanBasePackages = {"com.example.lms", "com.example.lms.model"})
+
 public class LMSApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LMSApplication.class, args);
