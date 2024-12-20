ca2f658e1c8834d1dc2a47001a1874bb9baf2d1d
yousef-showman <123179662+yousef-showman@users.noreply.github.com>
2024-12-20 11:24:59 +0200

    changed the table name

--- src/main/java/com/example/lms/model/User.java
+++ src/main/java/com/example/lms/model/User.java
@@ -1,18 +1,14 @@
 package com.example.lms.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
+import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
 @Getter
 @Setter
 @Entity
+@Table(name = "Users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
